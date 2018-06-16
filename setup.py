6cb76a76b5af36d3f45bc35314c832cd5d2beeae
bartosz_sobol <barteks95@gmail.com>
2018-06-16 21:23:26 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as rdme:
+    long_description = rdme.read()
+
+setuptools.setup(
+    name="osc-controller",
+    version="0.1",
+    author="Bartosz Sobol",
+    author_email="bartoszmsobol@gmail.com",
+    description="Simple library for creating osc software in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gogo2/osc-controller",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
